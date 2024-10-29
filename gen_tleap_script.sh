a78aa958b6fa8cd1d9c37b8bf9dd2e77f39a88ed
ChrisOrtiz29 <82282744+ChrisOrtiz29@users.noreply.github.com>
2024-10-29 13:48:08 +0800

    gen_tleap_script_update.sh

--- gen_tleap_script.sh
+++ gen_tleap_script.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 input_prot_pdb=$1
-input_lig_pdb=$2
-output_in=$3
-output_top=$4
-output_crd=$5
-output_pdb=$6
-num_pos_ion=$7
-num_neg_ion=$8
+output_in=$2
+output_top=$3
+output_crd=$4
+output_pdb=$5
+num_pos_ion=$6
+num_neg_ion=$7
 
 cat > ${output_in} << EOF
 source leaprc.gaff
 source leaprc.protein.ff14SB
 source leaprc.water.tip3p
-loadamberparams /mnt/Tsunami_HHD/wendy/mmgbsa/3SVG/ODR.frcmod
-loadoff /mnt/Tsunami_HHD/wendy/mmgbsa/3SVG/ODR.lib
-protein = loadpdb ${input_prot_pdb}
-lig = loadpdb ${input_lig_pdb}
-model = combine {protein lig}
+model = loadpdb ${input_prot_pdb}
 solvatebox model TIP3PBOX 10 iso
 addIons model Na+ 0
 addIons model Na+ ${num_pos_ion}
