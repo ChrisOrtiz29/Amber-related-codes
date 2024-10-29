8402201dd9f5536859d95c5c9b75853624b0ebf4
ChrisOrtiz29 <82282744+ChrisOrtiz29@users.noreply.github.com>
2024-10-29 13:47:23 +0800

    gen_model_update.sh

--- gen_model.sh
+++ gen_model.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 input_prot_pdb=$1
-input_lig_pdb=$2
-output_in=$3
-output_top=$4
-output_crd=$5
-output_pdb=$6
+output_in=$2
+output_top=$3
+output_crd=$4
+output_pdb=$5
 
 cat > ${output_in} << EOF
 source leaprc.gaff
 source leaprc.protein.ff14SB
 source leaprc.water.tip3p
-loadamberparams /mnt/Tsunami_HHD/wendy/mmgbsa/3SVG/ODR.frcmod
-loadoff /mnt/Tsunami_HHD/wendy/mmgbsa/3SVG/ODR.lib
-protein = loadpdb ${input_prot_pdb}
-lig = loadpdb ${input_lig_pdb}
-model = combine {protein lig}
+model = loadpdb ${input_prot_pdb}
 solvatebox model TIP3PBOX 10 iso
 addIons model Na+ 0
 addIons model Cl- 0
